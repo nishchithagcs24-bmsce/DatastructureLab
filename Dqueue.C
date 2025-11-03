dd88372aca08e0254115c0d30b46d9605fe3d07c
nishchithagcs24-bmsce <nishchithag.cs24@bmsce.ac.in>
2025-11-03 09:25:41 +0530

    Create Dqueue.C

---
+++ Dqueue.C
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#define SIZE 10
+
+int deque[SIZE];
+int front = -1, rear = -1;
+
+void insertFront(int value) {
+    if ((front == 0 && rear == SIZE - 1) || (front == rear + 1)) {
+        printf("Deque is full\n");
+        return;
+    }
+
+    if (front == -1) {
+        front = rear = 0;
+    } else if (front == 0) {
+        front = SIZE - 1;
+    } else {
+        front--;
+    }
+
+    deque[front] = value;
+}
+
+void insertRear(int value) {
+    if ((front == 0 && rear == SIZE - 1) || (front == rear + 1)) {
+        printf("Deque is full\n");
+        return;
+    }
+
+    if (front == -1) {
+        front = rear = 0;
+    } else if (rear == SIZE - 1) {
+        rear = 0;
+    } else {
+        rear++;
+    }
+
+    deque[rear] = value;
+}
+
+void deleteFront() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+
+    printf("Deleted from front: %d\n", deque[front]);
+
+    if (front == rear) {
+        front = rear = -1;
+    } else if (front == SIZE - 1) {
+        front = 0;
+    } else {
+        front++;
+    }
+}
+
+void deleteRear() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+
+    printf("Deleted from rear: %d\n", deque[rear]);
+
+    if (front == rear) {
+        front = rear = -1;
+    } else if (rear == 0) {
+        rear = SIZE - 1;
+    } else {
+        rear--;
+    }
+}
+
+void display() {
+    if (front == -1) {
+        printf("Deque is empty\n");
+        return;
+    }
+
+    printf("Deque elements: ");
+    int i = front;
+    while (1) {
+        printf("%d ", deque[i]);
+        if (i == rear)
+            break;
+        i = (i + 1) % SIZE;
+    }
+    printf("\n");
+}
+
+int main() {
+    insertRear(10);
+    insertRear(20);
+    insertFront(5);
+    display();
+
+    deleteFront();
+    deleteRear();
+    display();
+
+    return 0;
+}
+
+
+
+
+
+
+
+//OUTPUT
+/*Deque elements: 5 10 20 
+Deleted from front: 5
+Deleted from rear: 20
+Deque elements: 10 
+*/
